eb3dfad31f305cbec0611a9b8387f52dda56d9fe
Dan Langan <danlangan@Dans-MBP.hsd1.il.comcast.net>
2022-11-09 00:29:40 -0600

    going testing for the application, prepping for migrations and database set up

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,12 +3,15 @@ import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import AuthContext from "../../context/AuthContext";
 import "./NavBar.css";
+import {KEY} from '../../localKey';
 
 const Navbar = () => {
   const { logoutUser, user } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
     <div className="navBar">
+
+      <p> {KEY} </p>
       <ul>
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
---
+++ frontend/src/localKey.js
@@ -0,0 +1,1 @@
+export const KEY = 'fhjdkaoeuafbnu7qo83a'
