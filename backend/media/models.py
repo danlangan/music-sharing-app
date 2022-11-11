676b041029a187ca1663ae4c67dd897a23b8a42d
Dan Langan <danlangan@Dans-MBP.hsd1.il.comcast.net>
2022-11-11 13:23:23 -0600

    adding the newest migration to the backend that needed to be done to allow for links to come through with the song

--- backend/media/models.py
+++ backend/media/models.py
@@ -11,3 +11,6 @@ class Media(models.Model):
     album_id = models.CharField(max_length=255, null=True)
     playlist = models.CharField(max_length=255, null=True)
     playlist_id = models.CharField(max_length=255, null=True)
+    track_link = models.CharField(max_length=255, null=True)
+    album_link = models.CharField(max_length=255, null=True)
+    playlist_link = models.CharField(max_length=255, null=True)
