299e1d1f7dd758be51ea03934aec29015f5f316e
Dan Langan <danlangan@Dans-MBP.hsd1.il.comcast.net>
2022-12-15 14:39:47 -0600

    built out both sides of the application, now need to add the relevant keys to the app to make sure that the APIs can go through. From there I will be adding the CSS styling and figuring out the best ways to view the data that you have transferred by mapping it on the users home page

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
-
+import { Switch } from 'antd';
 import axios from "axios";
 
 const HomePage = () => {
@@ -10,8 +10,9 @@ const HomePage = () => {
   const [user, token] = useAuth();
   const [medias, setMedias] = useState([]);
   const [mediaInfo, setMediaInfo] = useState('');
-  const [translatedMedia, setTranslatedMedia] = useState('');
-  const [appleMusicSearch, setAppleMusicSearch] = useState('');
+  const [toggle, setToggle] = useState(false);
+  // const [translatedMedia, setTranslatedMedia] = useState('');
+  // const [appleMusicSearch, setAppleMusicSearch] = useState('');
 
   useEffect(() => {
     const fetchMedia = async () => {
@@ -30,6 +31,7 @@ const HomePage = () => {
   }, [token]);
 
   function getMediaInfo() {
+    if (toggle === true) {
     debugger;
     const splitUrlScoped = mediaInfo.split('/');
     let readyMediaType = ''
@@ -54,9 +56,9 @@ const HomePage = () => {
       debugger;
       try{
         let response = await axios.get(`https://api.spotify.com/v1/${readyMediaType}/${readyMediaId}`);
-        let spotifyDataReturn = response.data;
-        // setTranslatedMedia(response.data);
         console.log(response.data)
+        let spotifyDataReturn = response.data;
+        getSpotifyApiData(spotifyDataReturn);
       } catch (error) {
         console.log(error.message);
       };
@@ -64,43 +66,105 @@ const HomePage = () => {
       function getSpotifyApiData(spotifyDataReturn) {
         if (readyMediaType === 'tracks') {
           let parsedSpotifyData = `'mediaName' : ${spotifyDataReturn.href}`
+          queryAppleMusic(parsedSpotifyData);
       } else if (readyMediaType === 'playlists') {
           let parsedSpotifyData = `'mediaName' : ${spotifyDataReturn.href}`
+          queryAppleMusic(parsedSpotifyData);
       } else if (readyMediaType === 'albums') {
-        let parsedSpotifyData = `'mediaName' : ${spotifyDataReturn.href}`
+          let parsedSpotifyData = `'mediaName' : ${spotifyDataReturn.href}`
+          queryAppleMusic(parsedSpotifyData);
       };
-      queryAppleMusic(parsedSpotifyData);
+      
     };
     async function queryAppleMusic(parsedSpotifyData) {
       try{
-        let response = await axios.get(`https://api.music.apple.com/v1/me/library/search/${parsedSpotifyData.artists.name}+${parsedSpotifyData.name}`);
-        let appleMusicDataReturn = response.data
-        // setAppleMusicSearch(response.data);
+        let response = await axios.get(`https://api.music.apple.com/v1/me/library/search/${parsedSpotifyData.href.artists.name}+${parsedSpotifyData.href.name}`);
+        let appleMusicDataReturn = response.data;
         console.log(response.data);
+        console.log(appleMusicDataReturn);
       } catch (error) {
         console.log(error.message);
       };
     };
+  }
+  }
+  else if (toggle === false) {
+    debugger;
+    const splitUrlScoped = mediaInfo.split('/');
+    let readyMediaType = ''
+    let readyMediaId = ''
+    selectMediaId(splitUrlScoped);
+    selectMediatype(splitUrlScoped);
+    function selectMediatype(splitUrlScoped) {
+      if (splitUrlScoped[4] === 'song') {
+        readyMediaType = 'songs';
+      } else if (splitUrlScoped[4] === 'album') {
+        readyMediaType = 'albums';
+      } else if (splitUrlScoped[4] === 'playlist') {
+        readyMediaType = 'playlists'
+      };
+    };
+    function selectMediaId(splitUrlScoped) {
+      readyMediaId = splitUrlScoped[5];
+    }
+    fetchAppleMusicMediaData(readyMediaType, readyMediaId);
+
+    async function fetchAppleMusicMediaData(readyMediaType, readyMediaId) {
+      try{
+        let response = await axios.get(`https://api.music.apple.com/v1/us/${readyMediaType}/${readyMediaId}`)
+        console.log(response.data)
+        let appleMusicDataReturn = response.data;
+        console.log(appleMusicDataReturn);
+        getAppleMusicApiData(appleMusicDataReturn);
+      } catch (error) {
+        console.log(error.message)
+      }
+    }
+    function getAppleMusicApiData(appleMusicDataReturn) {
+      if (readyMediaType === 'tracks') {
+        let parsedAppleData = `'mediaName' : ${appleMusicDataReturn.href}`
+        querySpotify(parsedAppleData);
+    } else if (readyMediaType === 'playlists') {
+        let parsedAppleData = `'mediaName' : ${appleMusicDataReturn.href}`
+        querySpotify(parsedAppleData);
+    } else if (readyMediaType === 'albums') {
+        let parsedAppleData = `'mediaName' : ${appleMusicDataReturn.href}`
+        querySpotify(parsedAppleData);
+    };
   };
+  async function querySpotify(parsedAppleData, splitUrlScoped) {
+    try {
+      let response = await axios.get(`https://api.spotify.com/v1/search?type=${splitUrlScoped[4]},${parsedAppleData.name}_${parsedAppleData.artists}`);
+        console.log(response.data)
+        let spotifyDataReturn = response.data;
+        console.log(spotifyDataReturn);
+    } catch (error) {
+      console.log(error.message)
+    }
+  };
+
+  }
   //this is where getMediaInfo() ends
   // there is lots of building out to do left in the code above but that is the overall structure as to how to know what dot notation to use to access certain information within the variables
-   
   };
-
-    
-
-  
-
 
   function handleSubmit(event) {
     event.preventDefault();
     getMediaInfo(mediaInfo);
+  }
+  const spotifyOrApple = () => {
+    toggle ? setToggle(false): setToggle(true);
   }
 
   return (
     <div className="container">
       <h1>Welcom home, {user.username}!</h1>
       <form onSubmit={handleSubmit}>
+        <div>
+        <p>Toggle between sharing Spotify or an Apple Music Media</p>
+        <Switch onClick={spotifyOrApple}/>
+        {toggle ? <span>Spotify</span> : <span>Apple Music</span>}
+        </div>
       <input placeholder='Paste sharing link here' value={mediaInfo} onChange={(event) => setMediaInfo(event.target.value)}></input>
       <button type='submit'>Generate Sharing Capability</button>
       </form>
